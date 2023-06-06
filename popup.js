29d1838c35b751d327717d73a19ac3b35de3a195
Alexbcoyne <alexbcoyne@gmail.com>
2023-06-05 21:23:30 -0400

    correct formatting issue

--- popup.js
+++ popup.js
@@ -68,5 +68,8 @@ function createHeatmap(heatmapData) {
 }
 
 function formatDate(date) {
-    return new Date(date * 1000).toISOString().split('T')[0];
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 }
