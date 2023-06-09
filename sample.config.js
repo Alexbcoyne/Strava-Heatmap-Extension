6192ac104e8e28ce7ce05b65276653aeea1d7241
Alexbcoyne <alexbcoyne@gmail.com>
2023-06-09 13:13:54 -0400

    add sample refreshToken, fix spelling

--- sample.config.js
+++ sample.config.js
@@ -1,7 +1,8 @@
 const config = {
     accessToken: '<ACCESS_TOKEN>',
     clientId: 'CLIENT_ID',
-    clientSecret: 'CLIENT_SECTREY',
+    clientSecret: 'CLIENT_SECRET',
+    refreshToken: 'REFRESH_TOKEN',
 };
 
 window.config = config;
