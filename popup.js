ed696e8488b542558d207335e609a7a9431c076c
Alexbcoyne <alexbcoyne@gmail.com>
2023-06-15 10:32:06 -0400

    populate cell data with corrected color range

--- popup.js
+++ popup.js
@@ -30,8 +30,8 @@ function createHeatmap(heatmapData) {
     console.log(heatmapData);
 
     // heatmap dimensions
-    const width = 570;
-    const height = 120;
+    const width = 600;
+    const height = 125;
 
     // create svg
     const svg = d3.select('#heatmap').append('svg').attr('width', width).attr('height', height);
@@ -40,14 +40,15 @@ function createHeatmap(heatmapData) {
     const options = {
         cellSize: 10,
         cellPadding: 1,
-        startDate: new Date().getFullYear() + '-01-01',
-        endDate: new Date().getFullYear() + '-12-31',
-        colorRange: ['#DBDBDB', '#FFD3C0', '#FFA37C', '#FF7E46', '#FC4C02'],
+        startDate: getOneYearAgoDate(),
+        endDate: new Date(),
+        colorRange: ['#FFD3C0', '#FFBA9C', '#FFA37C', '#FF8C5C', '#FF7E46', '#FF6B2B', '#FC4C02'],
         tooltipUnit: 'minutes'
     };
 
+    const minCount = Math.min(...heatmapData.map(d => d.count));
     const maxCount = Math.max(...heatmapData.map(d => d.count));
-    
+
     // build calendar heatmap
     svg.selectAll("rect")
         .data(d3.timeDays(new Date(options.startDate), new Date(options.endDate)))
@@ -56,21 +57,26 @@ function createHeatmap(heatmapData) {
         .attr("width", options.cellSize - options.cellPadding)
         .attr("height", options.cellSize - options.cellPadding)
         .attr("x", function (d) {
-            const weekIndex = d3.timeWeek.count(d3.timeYear(d), d);
             const dayIndex = d.getDay();
+            const weekIndex = d3.timeWeek.count(d3.timeYear(d), d);
             return weekIndex * (options.cellSize + options.cellPadding) + options.cellPadding;
         })
         .attr("y", function (d) {
-            const weekIndex = d3.timeWeek.count(d3.timeYear(d), d);
             const dayIndex = d.getDay();
+            const weekIndex = d3.timeWeek.count(d3.timeYear(d), d);
             return dayIndex * (options.cellSize + options.cellPadding) + options.cellPadding;
         })
         .attr("fill", function (d) {
             const date = formatDate(d);
             const dataPoint = heatmapData.find(data => formatDate(data.date) === date);
             const count = dataPoint ? dataPoint.count : 0;
-            const colorIndex = Math.floor((count / maxCount) * (options.colorRange.length - 1));
-            return options.colorRange[colorIndex];
+
+            if (count === 0) {
+                return "#DBDBDB";
+            } else {
+                const colorIndex = Math.floor(((count - minCount) / (maxCount - minCount)) * (options.colorRange.length - 1));
+                return options.colorRange[colorIndex];
+            }
         })
         .append("title")
         .text(function (d) {
@@ -79,6 +85,7 @@ function createHeatmap(heatmapData) {
             const count = dataPoint ? dataPoint.count : 0;
             return `${date}: ${count} ${options.tooltipUnit}`;
         });
+
 }
 
 function formatDate(epoch) {
@@ -87,4 +94,10 @@ function formatDate(epoch) {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
+}
+
+function getOneYearAgoDate() {
+    const today = new Date();
+    const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+    return oneYearAgo;
 }
